d644c3b038816abc33aa0a0df811b47efefc00f9
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2023-06-10 19:05:45 +0700

    Picker image done

--- app/src/main/java/com/rickyandrean/batiktflite/MainActivity.kt
+++ app/src/main/java/com/rickyandrean/batiktflite/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.rickyandrean.batiktflite
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import com.rickyandrean.batiktflite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bitmap: Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectBtn.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent, 100)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            val uri = data?.data
+            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+            binding.imageView.setImageBitmap(bitmap)
+        }
     }
 }
