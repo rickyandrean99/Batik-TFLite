ca7753a08d63f013ab67ad7a830dd47340e3a794
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2023-06-11 03:45:48 +0700

    Batik prediction done

--- app/src/main/java/com/rickyandrean/batiktflite/MainActivity.kt
+++ app/src/main/java/com/rickyandrean/batiktflite/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import com.rickyandrean.batiktflite.databinding.ActivityMainBinding
-import com.rickyandrean.batiktflite.ml.MobilenetV110224Quant
+import com.rickyandrean.batiktflite.ml.Batik
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.common.ops.NormalizeOp
 import org.tensorflow.lite.support.image.ImageProcessor
@@ -17,14 +17,12 @@ import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bitmap: Bitmap
+    private val labels = listOf("Cendrawasih", "Geblek Renteng", "Insang", "Kawung", "Mega Mendung", "Parang", "Pring Sdapur", "Simbut", "Sogan", "Truntum")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Label
-        val labels = application.assets.open("labels.txt").bufferedReader().readLines()
 
         // Preprocess Image Taken
         val imagePreprocessing = ImageProcessor.Builder()
@@ -40,26 +38,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.predictBtn.setOnClickListener {
-            var tensorImage = TensorImage(DataType.UINT8)
+            // Load and preprocess the image
+            var tensorImage = TensorImage(DataType.FLOAT32)
             tensorImage.load(bitmap)
             tensorImage = imagePreprocessing.process(tensorImage)
 
-            val model = MobilenetV110224Quant.newInstance(this)
-            
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            // Prepare the model and input
+            val model = Batik.newInstance(this)
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
             inputFeature0.loadBuffer(tensorImage.buffer)
 
+            // Process model and get the result
             val outputs = model.process(inputFeature0)
             val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
 
+            // Show the result
             var maxIdx = 0
             outputFeature0.forEachIndexed { index, fl ->
                 if (outputFeature0[maxIdx] < fl) {
                     maxIdx = index
                 }
             }
-
             binding.resView.text = labels[maxIdx]
+
 
             model.close()
         }
