43d9b1fc84afe2b12490d4b67aa5e71d3784d501
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2023-06-11 02:42:50 +0700

    Prediction Done

--- app/src/main/java/com/rickyandrean/batiktflite/MainActivity.kt
+++ app/src/main/java/com/rickyandrean/batiktflite/MainActivity.kt
@@ -6,6 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import com.rickyandrean.batiktflite.databinding.ActivityMainBinding
+import com.rickyandrean.batiktflite.ml.MobilenetV110224Quant
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.ops.NormalizeOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,11 +23,45 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Label
+        val labels = application.assets.open("labels.txt").bufferedReader().readLines()
+
+        // Preprocess Image Taken
+        val imagePreprocessing = ImageProcessor.Builder()
+            .add(NormalizeOp(0.0f, 1.0f))
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+            .build()
+
         binding.selectBtn.setOnClickListener {
             val intent = Intent()
             intent.action = Intent.ACTION_GET_CONTENT
             intent.type = "image/*"
             startActivityForResult(intent, 100)
+        }
+
+        binding.predictBtn.setOnClickListener {
+            var tensorImage = TensorImage(DataType.UINT8)
+            tensorImage.load(bitmap)
+            tensorImage = imagePreprocessing.process(tensorImage)
+
+            val model = MobilenetV110224Quant.newInstance(this)
+            
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+            inputFeature0.loadBuffer(tensorImage.buffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+            var maxIdx = 0
+            outputFeature0.forEachIndexed { index, fl ->
+                if (outputFeature0[maxIdx] < fl) {
+                    maxIdx = index
+                }
+            }
+
+            binding.resView.text = labels[maxIdx]
+
+            model.close()
         }
     }
 
